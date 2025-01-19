c86550efb00950d5bd1177b640e7784038d57ad9
Ben Petlach <benpetlach@Bens-MacBook-Pro.local>
2025-01-18 23:51:37 -0500

    added hardware code

---
+++ Hardware/final/accel_track_and_pub/credentials.h.example.h
@@ -0,0 +1,17 @@
+// credentials.h.example
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// WiFi credentials
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+// MQTT Broker settings
+const char* mqtt_server = "YOUR_MQTT_SERVER";
+const int mqtt_port = YOUR_MQTT_PORT;
+const char* mqtt_username = "YOUR_MQTT_USERNAME";
+const char* mqtt_password = "YOUR_MQTT_PASSWORD";
+const char* client_id = "YOUR_CLIENT_ID";
+const char* topic = "YOUR_TOPIC";
+
+#endif
